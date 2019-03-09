d6dd43343360f8a5b9b8e5bb8c1b405c338f64b6
celinechole <github@celinechole.com>
2019-03-09 13:58:05 -0500

    implement fraction character

--- background.js
+++ background.js
@@ -1,3 +1,4 @@
+/* eslint-disable default-case */
 /* eslint-disable complexity */
 // var convert = require('convert-units')
 
@@ -17,7 +18,6 @@ function buttonClicked() {
   } else {
     chrome.browserAction.setBadgeText({text: ''})
     // chrome.browserAction.setIcon({path: 'Cooking-icon-bw-32.png'})
-    
   }
 }
 
@@ -34,6 +34,10 @@ function receiver(request, sender, sendResponse) {
   let unit = sanitizedString.replace(/[^a-zA-Z]/g, "");
   let value = sanitizedString.replace(/[^/\d]/g, "");
 
+  if (value === null) {
+    convertedMeasure = "Can't convert"o
+  }
+
   let split = value.split('/');
   if (split.length > 1) {
     value = split[0] / split[1];
@@ -41,19 +45,43 @@ function receiver(request, sender, sendResponse) {
   
   value = parseFloat(value);
 
+  switch (value) {
+    case '¾':
+      value = 0.75;
+      break;
+    case '½':
+      value = 0.5;
+      break;
+    case '¼':
+      value = 0.25
+      break;
+    default:
+    convertedMeasure = "Can't convert"
+  }
+
   switch (unit) {
     case "cup":
+    case "cups":
       convertedMeasure = Math.round(value * 236.588);
       convertedMeasure = `${convertedMeasure} mL`;
       break;
     case "pound":
+    case "pounds":
     case "lb":
+    case "lbs":
       convertedMeasure = Math.round(value * 453.592);
       convertedMeasure = `${convertedMeasure} grams`;
       break;
     case "ounce":
+    case "ounces":
       convertedMeasure = Math.round(value * 28.3495);
       convertedMeasure = `${convertedMeasure} grams`;
+      break;
+      
+    case "quart":
+    case "quarts":
+      convertedMeasure = Math.round(value * 946.353);
+      convertedMeasure = `${convertedMeasure} mL`;
       break;
 
     default:

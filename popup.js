681915bb5c89ae57f38f6bfd1b319bc2747e964e
celinechole <github@celinechole.com>
2019-03-09 11:56:58 -0500

    remove popup files

--- popup.js
+++
@@ -1,7 +0,0 @@
-var bgPage = chrome.extension.getBackgroundPage()
-
-const popup = document.querySelector('#measure')
-let test = document.createElement('h1')
-test.textContent = bgPage.getConvertedMeasure()
-popup.appendChild(test)
-
